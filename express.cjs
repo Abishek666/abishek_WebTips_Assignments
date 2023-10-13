cb7be5de7982aa2df2078bde93b8f6355558c76d
Abishek Thirupathi <abishek.thirupathi@solitontech.local>
2023-10-13 10:16:21 +0530

    feat:t3002

---
+++ express.cjs
@@ -0,0 +1,25 @@
+const express = require('express')
+const path = require('path')
+const timezone = require('./src/timeZone.cjs')
+
+const app = express()
+
+let allCityData = ''
+
+app.use(express.static(path.join(__dirname, '/src')))
+app.use(express.static(__dirname))
+app.use(express.json())
+app.get('/all-city-timezones', function (req, res) {
+  allCityData = timezone.allTimeZones()
+  res.json(allCityData)
+})
+
+app.post('/get-hourly-details', function (req, res) {
+  const postData = req.body
+  const citydn = postData.city_Date_Time_Name
+  const hours = postData.hours
+  const data = timezone.nextNhoursWeather(citydn, hours, allCityData)
+  res.json(data)
+})
+
+app.listen(5000, () => console.log('app running in port 5000'))
